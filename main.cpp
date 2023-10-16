81fb8ca6c455a3cfc355cfc61be174292950ef81
David Gini <dadlab.irl@gmail.com>
2023-10-16 12:12:38 +0200

    Variables de départ initialisées

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <barrier>
 
 using namespace std;
+
+barrier placesAssises(4);
+mutex aBord;
+int gardes = 0;
+int criminels = 0;
+mutex fileGardes;
+mutex fileCriminels;
+bool chauffeurPret = false;
+
+void initMutex() {
+    aBord.lock();
+}
+
 
 int main()
 {
